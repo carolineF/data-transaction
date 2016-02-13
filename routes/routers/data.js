879e866122f0345101a8ae22e45c3ddac5d6747b
carolineF <1138483832@qq.com>
2016-02-13 21:00:16 +0800

    add router of data upload

--- routes/routers/data.js
+++ routes/routers/data.js
@@ -3,4 +3,9 @@
 var express = require('express');
 var router = express.Router();
 
+var DataController = require('../../controllers/data');
+var dataController = new DataController();
+
+router.get('/upload', dataController.upload);
+
 module.exports = router;
