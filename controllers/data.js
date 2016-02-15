8e4a83b0bee94c5cc50dfaf2e1435672d7a313c3
carolineF <1138483832@qq.com>
2016-02-15 21:26:29 +0800

    add index method in dataController

--- controllers/data.js
+++ controllers/data.js
@@ -6,4 +6,8 @@ DataController.prototype.upload = function(req, res) {
   res.render('upload');
 };
 
+DataController.prototype.index = function(req, res){
+  res.render('data-info');
+};
+
 module.exports = DataController;
